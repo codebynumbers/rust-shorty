6d96353c1ee76de6ff3f4496c2fe53fd21d216d8
Rob Harrigan <rharrigan@50onred.com>
2020-02-05 19:20:19 -0500

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
-extern crate pruefung;
 use std::hash::Hasher;
 use rocket::request::Form;
+use pruefung::fnv::fnv32::Fnv32a;
 
 #[derive(FromForm)]
 struct Site {
@@ -24,8 +24,7 @@ fn expand(tag: String) -> String {
 #[get("/data/shorten?<site..>")]
 fn shorten(site: Option<Form<Site>>) -> String {
     if let Some(site) = site {
-        println!("{}", site.url);
-        let mut hasher = pruefung::fnv::fnv32::Fnv32a::default();
+        let mut hasher = Fnv32a::default();
         hasher.write(site.url.as_bytes());
         let hash = hasher.finish();
         format!("{:x}", hash)
